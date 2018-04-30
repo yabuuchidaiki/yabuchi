cfd3eb7a8723a47a8a73c5e2ad266fe1cf735988
WEB089-PC\WEB089 <yabuchi.seraku@gmail.com>
2018-05-01 08:52:16 +0900

    level5コーディング完了

--- Level5_cording/html/js/js1.js
+++ Level5_cording/html/js/js1.js
@@ -2,6 +2,12 @@ $(document).ready(function(){
     $('#slider').slick({
         arrows: false,
         dots: true
-        
+
+})
+
+$('#slider2').slick({
+    arrows: false,
+    dots: true
+
 })
 });
