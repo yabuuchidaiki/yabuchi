70a8e97cf656f0c9c0050060e1a0f687bd7b2a2b
WEB089-PC\WEB089 <yabuchi.seraku@gmail.com>
2018-05-15 14:23:46 +0900

    課題３　課題４　済

--- js課題/js/js1.js
+++ js課題/js/js1.js
@@ -14,7 +14,66 @@ $(function(){
 //課題２
 
 	$(function(){
-		$("#acMenu dt").on("click", function() {
+		$("#acmenu dt").on("click", function() {
 			$(this).next().slideToggle();
 		});
 	});
+
+  //課題３
+
+  $(function(){
+    $("#modal-open").click(function(){
+    $( this ).blur() ;
+    if( $( "#modal-overlay" )[0] ) return false ; 
+    
+    $( "body" ).append( '<div id="modal-overlay"></div>' ) ;
+    $( "#modal-overlay" ).fadeIn( "slow" ) ;
+
+    centeringModalSyncer() ;
+
+    $( "#modal-content" ).fadeIn( "slow" ) ;
+
+    $( "#modal-overlay,#modal-close" ).unbind().click( function(){
+
+    $( "#modal-content,#modal-overlay" ).fadeOut( "slow" , function(){
+    
+    $('#modal-overlay').remove() ;
+    
+    });
+    
+  });
+    
+});
+    
+    $( window ).resize( centeringModalSyncer ) ;
+
+    function centeringModalSyncer() {
+
+    var w = $( window ).width() ;
+    var h = $( window ).height() ;
+    
+
+    var cw = $( "#modal-content" ).outerWidth( {margin:true} );
+    var ch = $( "#modal-content" ).outerHeight( {margin:true} );
+    var cw = $( "#modal-content" ).outerWidth();
+    var ch = $( "#modal-content" ).outerHeight();
+    
+
+    $( "#modal-content" ).css( {"left": ((w - cw)/2) + "px","top": ((h - ch)/2) + "px"} ) ;
+    
+    }
+    
+});
+
+   //課題4
+   $(function() {
+    $('.navToggle').click(function() {
+        $(this).toggleClass('active');
+ 
+        if ($(this).hasClass('active')) {
+            $('.globalMenuSp').addClass('active');
+        } else {
+            $('.globalMenuSp').removeClass('active');
+        }
+    });
+});
